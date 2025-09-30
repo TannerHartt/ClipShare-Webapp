481a1023626651e00fd7a72305c2a03d6a21fe73
Tanner Hart <tannerhart24@yahoo.com>
2025-09-30 09:01:58 -0700

    environemnt and dependency set up.

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from "@angular/fire/compat/storage";
+
+import { environment  } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ClipService } from './services/clip.service';
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule, // Firebase authentication service.
+    AngularFirestoreModule, // Angular firestore db module.
+    AngularFireStorageModule, // Angular firebase helper module.
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

4cf58c7dc2d56a88f7ef381784ede3fabd3a3137
Tanner Hart <tannerhart24@yahoo.com>
2025-09-30 22:05:50 -0700

    created dynamic authenticaton modal to handle both user actions

---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,22 @@
+
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ModalService} from "../../services/modal.service";
+
+@Component({
+  selector: 'app-auth-modal',
+  templateUrl: './auth-modal.component.html',
+})
+export class AuthModalComponent implements OnInit, OnDestroy {
+
+  constructor(public modal: ModalService) { }
+
+  ngOnInit(): void {
+    this.modal.register('auth'); // Registers the 'auth' modal in the modal array for toggling in the DOM.
+  }
+
+  ngOnDestroy() {
+    this.modal.unRegister('auth'); // Removes the 'auth' modal from the array and DOM.
+  }
+
+
+}

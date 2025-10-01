dd7906c044d701a9921e36eab9740f46d3768bf5
Tanner Hart <tannerhart24@yahoo.com>
2025-09-30 22:18:20 -0700

    turned each category of components into their own module for organization

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from './modal/modal.component';
+import { ModalService } from "../services/modal.service";
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
+import { AlertComponent } from './alert/alert.component';
+import { InputComponent } from './input/input.component';
+
+
+
+@NgModule({
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    AlertComponent,
+    InputComponent,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
+  ],
+  providers: [ModalService],
+})
+export class SharedModule { }

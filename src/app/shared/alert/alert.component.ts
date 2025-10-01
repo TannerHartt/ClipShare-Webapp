6ab5f260fc774eaeeb92014d33da5d1d03b9c8a7
Tanner Hart <tannerhart24@yahoo.com>
2025-09-30 23:18:20 -0700

    built a simple alert component

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css']
+  styleUrls: ]
 })
 export class AlertComponent implements OnInit {
+
+  @Input() bColor: string = 'bg-blue-500'; // Default background color
+  @Input() color: string = 'blue'; // Default text color
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get bgColor() {
+    return `bg-${this.color}-400`;
   }
 
 }

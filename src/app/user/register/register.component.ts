cbbf479c4d349caebb4317ee24573ac7c17d7bc1
Tanner Hart <tannerhart24@yahoo.com>
2025-09-30 22:09:11 -0700

    started to implement registration

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: []
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  name = new FormControl('', [Validators.minLength(4), Validators.required]);
+  email = new FormControl('', [Validators.email, Validators.required]);
+  age = new FormControl('',[Validators.required]);
+  password = new FormControl('', [Validators.required, Validators.minLength(5)]);
+  confirm_password = new FormControl('', [Validators.required]);
+  number = new FormControl('',[Validators.required]);
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    number: this.number
+  });
 
 }

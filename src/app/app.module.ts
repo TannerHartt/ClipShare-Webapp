3816d4ac707e8afb973353cf1151991f3390e113
Tanner Hart <tannerhart24@yahoo.com>
2025-10-05 22:33:59 -0700

    manage and upload components imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { VideoModule } from './video/video.module';
 
 import { environment  } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ManageComponent } from './video/manage/manage.component';
+import { UploadComponent } from './video/upload/upload.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+
 ];
 
 @NgModule({
@@ -20,12 +26,17 @@ const routes: Routes = [
     NavComponent,
     HomeComponent,
     AboutComponent,
-    ManageComponent,
+    UploadComponent,
+
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    VideoModule
   ],
   providers: [],
   bootstrap: [AppComponent]

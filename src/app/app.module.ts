b4ff54d90eaf3348689fc7b8462f3672476d3d91
Tanner Hart <tannerhart24@yahoo.com>
2025-10-05 22:44:02 -0700

    new route and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,15 @@ import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { UploadComponent } from './video/upload/upload.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ClipComponent } from './clip/clip.component';
+import { ClipsListComponent } from './clips-list/clips-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
 
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
@@ -27,6 +31,9 @@ const routes: Routes = [
     HomeComponent,
     AboutComponent,
     UploadComponent,
+    NotFoundComponent,
+    ClipComponent,
+    ClipsListComponent,
 
   ],
   imports: [

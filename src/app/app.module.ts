ae8423ee1f8720bc39b7057caf49d976e92e4539
Tanner Hart <tannerhart24@yahoo.com>
2025-10-05 21:15:56 -0700

    module reorganization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireStorageModule } from "@angular/fire/compat/storage";
 
 import { environment  } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
-import { TabComponent } from './shared/tab/tab.component';
-import { TabsContainerComponent } from './shared/tabs-container/tabs-container.component';
-import { ModalComponent } from './shared/modal/modal.component';
-import { AuthModalComponent } from './user/auth-modal/auth-modal.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-    { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     NavComponent,
-    TabComponent,
-    TabsContainerComponent,
-    ModalComponent,
-    AuthModalComponent,
-    LoginComponent,
-    RegisterComponent,
+    HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    // AngularFireModule.initializeApp(environment.firebase),
-    // AngularFireAuthModule, // Firebase authentication service.
-    // AngularFirestoreModule, // Angular firestore db module.
-    // AngularFireStorageModule, // Angular firebase helper module.
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

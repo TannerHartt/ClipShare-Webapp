f77217d83c9eb7b7dad2f514bf74cc894a087e6f
Tanner Hart <tannerhart24@yahoo.com>
2025-10-05 21:16:26 -0700

    fixed syntax error

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ]
+  styleUrls: []
 })
 export class AlertComponent implements OnInit {
 
